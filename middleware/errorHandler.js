7349344b762e2f090f99efaeaefda3642b018d30
William Chong <williamchong007@msn.com>
2018-11-02 21:10:03 +0800

    ✨ Add common middlewares and ValidationError class

---
+++ middleware/errorHandler.js
@@ -0,0 +1,17 @@
+import ValidationError from '../util/ValidationError';
+
+export default function errorHandler(err, req, res, next) {
+  const msg = (err.response && err.response.data) || err.message || err;
+  console.error(msg);
+  if (res.headersSent) {
+    return next(err);
+  }
+  if (err instanceof ValidationError) {
+    return res.status(400).send(msg);
+  }
+  // Handle multer error
+  if (err.code && err.code === 'LIMIT_FILE_SIZE') {
+    return res.status(400).send('FILE_TOO_LARGE');
+  }
+  return res.sendStatus(500);
+}
