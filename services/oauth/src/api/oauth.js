c945d4224d6afa17db67e1630e1a3f82c258ab66
William Chong <williamchong007@msn.com>
2018-11-09 20:01:24 +0800

    ✨ [oauth] Add oauth internal api service

---
+++ services/oauth/src/api/oauth.js
@@ -0,0 +1,103 @@
+import { Router } from 'express';
+
+import { fetchFacebookUser } from '../util/oauth/facebook';
+import { fetchFlickrOAuthInfo, fetchFlickrUser } from '../util/oauth/flickr';
+import { fetchMediumOAuthInfo, fetchMediumUser } from '../util/oauth/medium';
+import {
+  fetchTwitterOAuthInfo,
+  fetchTwitterUser,
+  fetchTwitterUserByAccessToken,
+} from '../util/oauth/twitter';
+import { ValidationError } from '../../shared/ValidationError';
+
+const router = Router();
+
+router.get('/oauth/facebook/user', async (req, res, next) => {
+  try {
+    const { token } = req.query;
+    if (!token) throw new ValidationError('missing token');
+    const result = await fetchFacebookUser(token);
+    res.json(result);
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.get('/oauth/flickr/info', async (req, res, next) => {
+  try {
+    const { user } = req.query;
+    if (!user) throw new ValidationError('missing user');
+    const result = await fetchFlickrOAuthInfo(user);
+    res.json(result);
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.get('/oauth/flickr/user', async (req, res, next) => {
+  try {
+    const { token, secret, verifier } = req.query;
+    if (!token || !secret || !verifier) throw new ValidationError('missing params');
+    const result = await fetchFlickrUser(token, secret, verifier);
+    res.json(result);
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.get('/oauth/medium/info', async (req, res, next) => {
+  try {
+    const { user } = req.query;
+    if (!user) throw new ValidationError('missing user');
+    const result = await fetchMediumOAuthInfo(user);
+    res.json(result);
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.get('/oauth/medium/user', async (req, res, next) => {
+  try {
+    const { code } = req.query;
+    if (!code) throw new ValidationError('missing code');
+    const result = await fetchMediumUser(code);
+    res.json(result);
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.get('/oauth/twitter/info', async (req, res, next) => {
+  try {
+    const { user } = req.query;
+    if (!user) throw new ValidationError('missing user');
+    const result = await fetchTwitterOAuthInfo(user);
+    res.json(result);
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.get('/oauth/twitter/user', async (req, res, next) => {
+  try {
+    const { token, secret, verifier } = req.query;
+    if (!token || !secret || !verifier) throw new ValidationError('missing params');
+    const result = await fetchTwitterUser(token, secret, verifier);
+    res.json(result);
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.get('/oauth/twitter/user/accesstoken', async (req, res, next) => {
+  try {
+    const { token, secret } = req.query;
+    if (!token || !secret) throw new ValidationError('missing params');
+    const result = await fetchTwitterUserByAccessToken(token, secret);
+    res.json(result);
+  } catch (err) {
+    next(err);
+  }
+});
+
+export default router;
---
+++ services/oauth/src/index.js
@@ -0,0 +1,24 @@
+import express from 'express';
+
+import api from './api/oauth';
+import errorHandler from '../shared/middleware/errorHandler';
+
+const app = express();
+const host = process.env.HOST || '127.0.0.1';
+const port = process.env.PORT || 3000;
+
+app.set('port', port);
+
+if (process.env.NODE_ENV === 'production') app.disable('x-powered-by');
+
+app.use(api);
+
+app.get('/', (req, res) => {
+  res.sendStatus(200);
+});
+
+app.use(errorHandler);
+
+app.listen(port, host);
+
+console.log(`Server listening on ${host}:${port}`); // eslint-disable-line no-console
